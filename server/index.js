f769ac5529824bcab92aa2808c12382e7cf5dd27
Austin Carly <austinrcarly16@gmail.com>
2020-09-17 15:54:21 -0600

    Server Set Up

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+const app = express();
+const {
+    CONNECTION_STRING,
+    SERVER_PORT,
+    SESSION_SECRET
+} = process.env;
+
+app.use(express.json());
+
+app.use(
+    session({
+      resave: false,
+      saveUninitialized: true,
+      cookie: { maxAge: 1000 * 60 * 60 * 48 },
+      secret: SESSION_SECRET,
+    })
+  );
+
+  massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false },
+  }).then((db) => {
+    app.set("db", db);
+    console.log(`Database is connected.`);
+  });
+
+
+
+
+  app.listen(SERVER_PORT, () =>
+  console.log(`You are connected to port ${SERVER_PORT}.`)
+);
