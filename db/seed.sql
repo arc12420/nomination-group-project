40e2963066c241d05de44b1099dbbf1a7347ded7
huntj0n <hunt.jon19@gmail.com>
2020-09-18 09:04:23 -0600

    altered contacts table

---
+++ db/seed.sql
@@ -0,0 +1,43 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    password TEXT,
+    profile_pic TEXT,
+    isAdmin BOOLEAN
+);
+
+CREATE TABLE donations (
+    donation_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id),
+    project_id INTEGER REFERENCES projects(project_id),
+    total INTEGER
+    -- or amount
+);
+
+CREATE TABLE projects (
+    project_id SERIAL PRIMARY KEY,
+    name TEXT,
+    date TEXT,
+    time TEXT,
+    location TEXT,
+    description TEXT,
+    donation_goal INTEGER
+);
+
+CREATE TABLE nominations (
+    nomination_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id),
+    name TEXT,
+    desctription TEXT
+    -- name of whoever is being nominated and description of why
+)
+
+CREATE TABLE contacts (
+    contact_id SERIAL PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    -- title? title TEXT,
+    phone TEXT
+);
