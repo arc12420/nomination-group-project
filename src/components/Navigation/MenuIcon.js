5ee88d21ddcc8c362a66cc031c42fc466e11d32d
Noal Brown <noalbrown1@gmail.com>
2020-09-18 17:35:16 -0500

    finished hamburger

---
+++ src/components/Navigation/MenuIcon.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+function MenuIcon(props) {
+  return (
+    <svg fill="none" viewBox="0 0 24 24" height="1em" width="1em" {...props}>
+      <path
+        fill="currentColor"
+        d="M2 6a1 1 0 011-1h18a1 1 0 110 2H3a1 1 0 01-1-1zM2 12.032a1 1 0 011-1h18a1 1 0 110 2H3a1 1 0 01-1-1zM3 17.064a1 1 0 100 2h18a1 1 0 000-2H3z"
+      />
+    </svg>
+  );
+}
+
+export default MenuIcon;
