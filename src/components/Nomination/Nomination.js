49d52c0a01c0f6babb3678b8f568a2e1aa1764f5
Austin Carly <austinrcarly16@gmail.com>
2020-09-18 11:37:06 -0600

    Page Layout Complete

--- src/components/Nomination/Nomination.js
+++ src/components/Nomination/Nomination.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React from "react";
+import "../Nomination/Nomination.css";
 
 const Nomination = () => {
-
   return (
-    <div>
-      <p>Nomination</p>
+    <div className="nominationComponent">
+      <p> <b>The Ivory Altruism Award</b> <br/>
+      This award goes to recognize amazing individuals within our communities. These are individuals who display great selflessness and giving even when it may be difficult.
+      </p>
+      <main className="nominationInputs">
+        First Name
+        <input className="" 
+        placeholder="First Name" 
+        // value={} 
+        />
+        Last Name
+        <input className="" 
+        placeholder="Last Name" 
+        // value={} 
+        />
+        Image Of Nominee
+        <input className="" 
+        placeholder="Image Of Nominee" 
+        // value={} 
+        />
+        Why do they deserve this award?
+        <textarea
+          className="whyTheyDeserve"
+          placeholder="Why do they deserve this award?"
+          // value={}
+        />
+        <button className="submitButton">Submit</button>
+      </main>
     </div>
-  )
-}
+  );
+};
 
 export default Nomination;
