67d3382e1638ddbc6286891a9b06ced0713ec996
bitsybaron <59240206+bitsybaron@users.noreply.github.com>
2020-09-18 14:06:18 -0600

    fixed bug in controller

--- server/postControllers.js
+++ server/postControllers.js
@@ -1,8 +1,8 @@
 module.exports = {
     getVolunteerProjects: async (req, res) => {
         const db = req.app.get('db');
-        const projects = await db.get_volunteer_projects;
-        console.log(projects)
+        const projects = await db.get_volunteer_projects();
+        
         res.status(200).send(projects);
     }
 }
