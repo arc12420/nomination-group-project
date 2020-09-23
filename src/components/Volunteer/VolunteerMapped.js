5bfaa1db6ac539bfa1b2f3441a2b5495250589c1
huntj0n <hunt.jon19@gmail.com>
2020-09-23 11:02:49 -0600

    events page styling

--- src/components/Volunteer/VolunteerMapped.js
+++ src/components/Volunteer/VolunteerMapped.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './volunteer.css';
 
+import Button from '@material-ui/core/Button'
+
+
 const VolunteerMapped = (props) => {
 
     const {proj} = props;
     return (
-        <div className='volunteer-project'>
-            <h4 className='proj-name'>{proj.name}</h4>
-            <p>{proj.date}</p>
-            <span>{proj.time}</span>
-            <p>{proj.description}</p>
-            {/* The button below will probably be hooked up to nodemailer or Twilio?*/}
-            <button>Sign Up for Text Reminders</button>
+        <div className='volunteer__projectCard' style={{backgroundImage: `url(${proj.photo})`}}>
+            <div className="volunteer__projectCardInfo">
+                <h4 className='proj-name'>{proj.name}</h4>
+                <span>{proj.date}</span>
+                <span>{proj.time}</span>
+                <p>{proj.description}</p>
+                {/* The button below will probably be hooked up to nodemailer or Twilio?*/}
+                <button>Sign Up for Text Reminders</button>
+            </div>
 
         </div>
     )
