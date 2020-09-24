784bcbd438a0cc65b710b49af5c4b32a4f816e33
bitsybaron <59240206+bitsybaron@users.noreply.github.com>
2020-09-24 11:07:17 -0600

    added styling

--- src/components/Donations/EventsMapped.js
+++ src/components/Donations/EventsMapped.js
@@ -3,10 +3,12 @@ import React from 'react';
 const EventsMapped = (props) => {
     const {proj} = props;
     return (
-        <div className='proj'>
+        <div className='proj' style={{backgroundImage: `url(${proj.photo})`}}>
+            <div className='.proj-info'>
             <h4 className='proj-name'>{proj.name}</h4>
             <p>{proj.date}</p>
             <span>{proj.time}</span>
+            </div>
             
             
 
--- src/components/Donations/ThankYou.js
+++ src/components/Donations/ThankYou.js
@@ -15,13 +15,15 @@ const ThankYou = () => {
   }, [])
 
     return(
-        <div className='thanks'>
+        <div className='thanks' >
             {state.isLoggedIn ? <h2>Thank you, {state.user.first_name}!</h2> : <h2>Thank your for your generosity!</h2>}
             <p>Your donation of ${state.donation.total/100} to the {state.donation.project_name} project is greatly appreciated. You'll receive an emailed receipt shortly.</p>
             <p>Check out our upcoming events!</p>
+            <div className='proj-container'>
             {volunteerProjects.map(proj => {
                 return <EventsMapped key={proj.project_id} proj={proj}/>
             })}
+            </div>
         </div>
     )
 }
