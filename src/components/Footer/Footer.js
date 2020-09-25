309036f87bd96208da8c23ec74ac48c2b081e965
huntj0n <hunt.jon19@gmail.com>
2020-09-25 16:20:28 -0600

    footer redesign

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,26 +5,30 @@ import './Footer.css'
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import InstagramIcon from '@material-ui/icons/Instagram';
-import YouTubeIcon from '@material-ui/icons/YouTube';
-import PinterestIcon from '@material-ui/icons/Pinterest';
+
 
 function Footer() {
     return (
         <div className="footer">
         
-        {/* <div className="footer__media">
-          <FacebookIcon/>
-          <TwitterIcon/>
-          <InstagramIcon/>
-          <YouTubeIcon/>
-          <PinterestIcon/>
-        </div> */}
+        <div className="footer__media">
+          <FacebookIcon style={{fontSize: '32px'}}/>
+          <InstagramIcon style={{fontSize: '32px'}}/>
+          <TwitterIcon style={{fontSize: '32px'}}/>
+        </div>
 
-        {/* <div className="footer__copyright">Copyright © {new Date().getFullYear()}, Ivory Foundation, no rights reserved, this is a demo</div> */}
-        <div className="footerCardOne"></div>
-        <div className="footerCardTwo"></div>
-        <div className="footerCardThree"></div>
-        <div className="footerCardFour"></div>
+        <div className="siteBy">
+          Site by (insert names here)
+        </div>
+
+        <div className="footer__links">
+          <p>Info</p>
+          <p>Support</p>
+          <p>Request a Speaker</p>
+          <p>Covid 19 Response</p>
+        </div>
+
+        <div className="footer__copyright">Copyright © {new Date().getFullYear()}, Ivory Foundation, no rights reserved, this is a demo</div>
 
       </div>
     )
