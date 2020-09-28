0404a3541970f33b208f1e6d3a211d8a09f105dc
Noal Brown <noalbrown1@gmail.com>
2020-09-28 11:44:21 -0500

    About page styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,12 +4,33 @@ import './about.css'
 const About = () => {
 
   return (
-    <div className='about'>
-      <h2>About The Ivory Foundation</h2>
-      <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
-      dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
-      Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
-    sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.</p>
+    <div>
+      <section className='about'>
+        <h2>About The Ivory Foundation</h2>
+        <p>The Ivory Foundation is working with local communities to empower people to serve others. The foundation enables members of the community access to service projects. The Ivory Foundation received over 150 Million dollars in donations in which 100% of the funds went directly back into its people to help strengthen and unify the communities.
+          <hr />
+            Back in 2004 our founder JANE Dev, was blessed to be able to go with many others to assist in providing aid to the people of Haiti immediately following hurricane Ike. She was completely astounded at the number of volunteers that had traveled to render aid but even more than that, she was humbled to see the people of Haiti serving each other. “Even after all they had been through and all that they had lost, they were there to help each other.” - JANE Dev
+        <hr />
+              Our non-profit organizations core motivation stems from the quote “Small acts, when multiplied by millions of people, can transform the world.” - Howard Zinn
+        <hr />
+                Please join us in transforming our world, one community at a time.</p>
+      </section>
+      <section>
+        <div class="area" >
+          <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div >
+      </section>
     </div>
   )
 }
