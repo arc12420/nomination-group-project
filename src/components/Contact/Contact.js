0ce44e02a8fcf063e5033d896825e9d5874342c6
Noal Brown <noalbrown1@gmail.com>
2020-10-01 09:58:49 -0500

    button omissions and general clean up

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import '../Contact/contact.css';
 
 const Contact = () => {
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -13,16 +13,16 @@ const Welcome = (props) => {
     email: ''
   });
 
-  const handleInput = ( event ) => {
-    setNewsletterInput({...newsletterInput, [event.target.name]: event.target.value})
+  const handleInput = (event) => {
+    setNewsletterInput({ ...newsletterInput, [event.target.name]: event.target.value })
   }
 
   const signUp = () => {
     setNewsletterInput({
-      name: '', 
-      email:''
+      name: '',
+      email: ''
     })
-    if(newsletterInput.name && newsletterInput.email !== ''){
+    if (newsletterInput.name && newsletterInput.email !== '') {
       alert('Thank you for joining our newsletter!')
     } else {
       alert('Please enter a valid email address')
@@ -36,34 +36,34 @@ const Welcome = (props) => {
         <div className="welcome__jumbotronTitle">
           <h1>KINDNESS IS RISING</h1>
           <h5>Living Through Giving</h5>
-          <button onClick={()=> props.history.push('/about')}>LEARN MORE</button>
+          {/* <button onClick={() => props.history.push('/about')}>LEARN MORE</button> */}
           <div className="welcome__jumbotronTitleCards">
             <div className="titleCard">
               <p>Take The Pledge</p>
-              <img src={require('../../images/Handshake.png')} alt="" style={{ width: '110px' , marginTop: '20px'}}/>
+              <img src={require('../../images/Handshake.png')} alt="" style={{ width: '110px', marginTop: '20px' }} />
             </div>
             <div className="titleCard">
               <p>Start Your Project</p>
-              <img src={require('../../images/74-512.png')} alt="" style={{ width: '85px' }}/>
+              <img src={require('../../images/74-512.png')} alt="" style={{ width: '85px' }} />
             </div>
             <div className="titleCard">
               <p>Show Your Impact</p>
-              <img src={require('../../images/award.png')} alt="" style={{ width: '80px' }}/>
+              <img src={require('../../images/award.png')} alt="" style={{ width: '80px' }} />
             </div>
           </div>
         </div>
       </div>
 
-      <div className="welcome__row" style={{ height: '75vh', display:'flex', alignItems:'center', justifyContent:'center'}}>
+      <div className="welcome__row" style={{ height: '75vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <div className="welcome__tiles">
 
-          <div className="tile__card tileOne" onClick={()=> history.push('/nomination')}>nominated community members</div>
+          <div className="tile__card tileOne" onClick={() => history.push('/nomination')}>nominated community members</div>
 
-          <div className="tile__card tileTwo" onClick={()=> history.push('/volunteer')}> Volunteer</div>
-          
-          <div className="tile__card tileThree" onClick={()=> history.push('/contact')}> Partner resources</div>
-            
-        <div className="tile__card tileFour" onClick={()=> history.push('/donations')}>virtual fundraiser</div>
+          <div className="tile__card tileTwo" onClick={() => history.push('/volunteer')}> Volunteer</div>
+
+          <div className="tile__card tileThree" onClick={() => history.push('/contact')}> Partner resources</div>
+
+          <div className="tile__card tileFour" onClick={() => history.push('/donations')}>virtual fundraiser</div>
 
         </div>
       </div>
@@ -76,7 +76,7 @@ const Welcome = (props) => {
             <h1>
               We've funded 56,759 tomato projects for over 11 million people around the world.
             </h1>
-            <button>SEE MORE OF OUR IMPACT</button>
+            {/* <button>SEE MORE OF OUR IMPACT</button> */}
           </div>
         </div>
       </div>
@@ -86,11 +86,11 @@ const Welcome = (props) => {
           <h2>Join our Newsletter</h2>
 
           <div className="welcome__newsletterInputs">
-            <input name="name" value={newsletterInput.name} type="text" onChange={handleInput} placeholder="Name" required/>
-            <input name="email" value={newsletterInput.email} type="email" onChange={handleInput} placeholder="Email" required/>
+            <input name="name" value={newsletterInput.name} type="text" onChange={handleInput} placeholder="Name" required />
+            <input name="email" value={newsletterInput.email} type="email" onChange={handleInput} placeholder="Email" required />
           </div>
 
-          <button onClick={ signUp }>Sign Up Today</button>
+          <button onClick={signUp}>Sign Up Today</button>
 
 
         </div>
